33d04735c48d19f6981df2f53ba1373edfe34118
Jonathan Q <jonathan.quekww@gmail.com>
2024-08-24 21:51:20 +0800

    chore: import mui/x-charts for analytics

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,8 +1,19 @@
+import { LineChart } from "@mui/x-charts";
+
 export default function AnalyticsPage({ user, setUser }) {
-    return (
-      <>
-        <h1>analytics page is here</h1>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <h1>analytics page is here</h1>
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        width={500}
+        height={300}
+      />
+    </>
+  );
+}
