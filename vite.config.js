91289a5f37c7f00641fd6030596a50308b646baf
Jonathan Q <jonathan.quekww@gmail.com>
2024-08-25 23:35:17 +0800

    fix:re-establish correct backend proxy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5001",
+      "/api": "https://fruits-backend.onrender.com",
     },
   },
   plugins: [react()],
